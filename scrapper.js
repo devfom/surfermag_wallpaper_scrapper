df8d0ad1399988226e62beb2c57e11a3f09bc0ae
Vasily Fomin <vasili.fomin@gmail.com>
2014-07-12 22:56:33 +0200

    Notify only about file saved event. Less hardcoded strings

--- scrapper.js
+++ scrapper.js
@@ -5,28 +5,22 @@ var cheerio = require('cheerio')
 var path = require('path')
 
 var url = 'http://www.surfermag.com/wallpapers/'
-var saveDirectory = '/home/devfom/Pictures/surfing/test'
+var saveDirectory = process.env['HOME'] + '/Pictures/surfing/test'
+var wallpaperSize = '1600x1200'
 
 request(url, function (error, response, html) {
 	if(!error){
 		var $ = cheerio.load(html)	
 		$('.wallpapers').find('a').filter(function(index, element) {			
-			return $(this).text() == '1600x1200'
+			return $(this).text() == wallpaperSize
 		}).each(function(i, hrefElement) {				
 			var downloadLink = $(this).attr('href')
 			var fileName = path.basename(downloadLink)
-			var saveFilePath = path.join(saveDirectory, fileName)
-			console.log("Downloading file " + fileName)
+			var saveFilePath = path.join(saveDirectory, fileName)			
 
-			progress(request(downloadLink), {
-				throttle: 1000,
-				delay: 1000
-			}).on('progress', function(state) {
-				console.log('percent ', state.percent)
-			}).on('error', function(err) {
-				console.log('Something went wrong')
-			}).pipe(fs.createWriteStream(saveFilePath)).on('close', function(err){
-				console.log('File saved')
+			progress(request(downloadLink))
+			.pipe(fs.createWriteStream(saveFilePath)).on('close', function(err){				
+				console.log('File saved: ' + this.path)
 			})
 		})
 	};
